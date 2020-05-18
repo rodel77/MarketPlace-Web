bcc5530a9fcb4fce4ce75142b06c3be74c237b9d
rodel <rodelbusiness@hotmail.com>
2020-05-18 16:15:07 -0500

    Fixed encoding issues

--- src/db_manager.php
+++ src/db_manager.php
@@ -1,7 +1,7 @@
 <?php
     function db_connect(){
         if(!isset($GLOBALS["currentConnection"])){
-            $GLOBALS["currentConnection"] = new PDO("mysql:host=".DB_HOST.";dbname=".DB_DATABASE, DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $GLOBALS["currentConnection"] = new PDO("mysql:host=".DB_HOST.";dbname=".DB_DATABASE.";charset=utf8", DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         }
 
         return $GLOBALS["currentConnection"];
